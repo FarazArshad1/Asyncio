215732e96991d58cf65995a6cfaaca2a73cac64e
FarazArshad1 <newsletters833@gmail.com>
2024-12-24 15:25:42 +0500

    Running Coroutines example

---
+++ lesson01_call_warning.py
@@ -0,0 +1,9 @@
+# example of calling a coroutine directly
+
+# define a coroutine
+async def custom_coroutine():
+	# report a message
+    print('Hello world')
+
+# call the coroutine directly
+custom_coroutine() # raises warning
